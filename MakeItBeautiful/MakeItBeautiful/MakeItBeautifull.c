14cb2f8688e051edc0ed4081831fb419070044f7
Tentacule23 <mad_ol_man@hotmail.com>
2014-02-01 22:00:21 -0500

    01/02/2014 10:00pm

--- MakeItBeautiful/MakeItBeautiful/MakeItBeautifull.c
+++ MakeItBeautiful/MakeItBeautiful/MakeItBeautifull.c
@@ -18,73 +18,22 @@
 
 #include <stdio.h>
 
-int test2(double px, double py, double m, double b)
-{
-	if (py < m * px + b) {
-		return -1; // point is under line
-	}
-	else if (py == m * px + b){
-		return 0;
-	}
-	else {
-		return 1; // over
-	}
-}
+int test2(double pointToCheckx, double pointToChecky, double m, double b);
+int test1(double pointToCheckx, double pointToChecky, double m, double b, double lx, double ly);
+int tritest(double vertexAx, double vertexAy, double vertexBx, double vertexBy, double vertexCx, double vertexCy, double pointToCheckx, double pointToChecky);
 
-int // two points lie on the same side of a line
-test1(double px, double py, double m, double b, double lx, double ly) {
-	return (test2(px, py, m, b) == test2(lx, ly, m, b));
-}
-int tritest(double x0, double y0, double x1, double y1, double x2, double y2, double px, double py){
-
-	int line1, line2, line3;
-	// line eqns
-
-	double m01 = (y1 - y0) / (x1 - x0);
-	// b: y - y1 = m( x - x1 ), x = 0
-	double b01 = m01 * -x1 + y1;
-	double m02, m12, b02, b12;
-	m02 = (y2 - y0) / (x2 - x0);
-	m12 = (y2 - y1) / (x2 - x1);
-	b02 = m02 * -x2 + y2;
-	b12 = m12 * -x2 + y2;
-
-	// vertical line checks
-	if (x1 == x0) {
-		line1 = ((px <= x0) == (x2 <= x0));
-	}
-	else {
-		line1 = test1(px, py, m01, b01, x2, y2);
-	}
-
-	if (x1 == x2) {
-		line2 = ((px <= x2) == (x0 <= x2));
-	}
-	else {
-		line2 = test1(px, py, m12, b12, x0, y0);
-	}
-
-	if (x2 == x0) {
-		line3 = ((px <= x0) == (x1 <= x0));
-	}
-	else {
-		line3 = test1(px, py, m02, b02, x1, y1);
-	}
-
-	return line1 && line2 && line3;
-
-}
 int main(int argc, char* argv[]) {
-	double x0, y0, x1, y1, x2, y2, px;
-	double py;
+	
+	double vertexAx, vertexAy, vertexBx, vertexBy, vertexCx, vertexCy;
+	double pointToCheckx, pointToChecky;
 
 	int scanfsReturnValueAggregatedOverAllScanfs = 0;
 
 	// get input
-	printf("Triangle Vertex A (x,y): "); scanfsReturnValueAggregatedOverAllScanfs += scanf("%lf,%lf", &x0, &y0);
-	printf("Triangle Vertex  B (x,y): "); scanfsReturnValueAggregatedOverAllScanfs += scanf("%lf,%lf", &x1, &y1);
-	printf("Triangle Vertex  C (x,y): "); scanfsReturnValueAggregatedOverAllScanfs += scanf("%lf,%lf", &x2, &y2);
-	printf("Test Point (x,y): "); scanfsReturnValueAggregatedOverAllScanfs += scanf("%lf,%lf", &px, &py);
+	printf("Triangle Vertex A (x,y): "); scanfsReturnValueAggregatedOverAllScanfs += scanf("%lf,%lf", &vertexAx, &vertexAy);
+	printf("Triangle Vertex  B (x,y): "); scanfsReturnValueAggregatedOverAllScanfs += scanf("%lf,%lf", &vertexBx, &vertexBy);
+	printf("Triangle Vertex  C (x,y): "); scanfsReturnValueAggregatedOverAllScanfs += scanf("%lf,%lf", &vertexCx, &vertexCy);
+	printf("Test Point (x,y): "); scanfsReturnValueAggregatedOverAllScanfs += scanf("%lf,%lf", &pointToCheckx, &pointToChecky);
 
 	// print error
 	if (scanfsReturnValueAggregatedOverAllScanfs != 8) {
@@ -93,11 +42,69 @@ int main(int argc, char* argv[]) {
 	}
 
 	// print answer
-	printf("Point (%.2lf,%.2lf) is ", px, py);
-	if (tritest(x0, y0, x1, y1, x2, y2, px, py)) printf("inside");
+	printf("Point (%.2lf,%.2lf) is ", pointToCheckx, pointToChecky);
+	if (tritest(vertexAx, vertexAy, vertexBx, vertexBy, vertexCx, vertexCy, pointToCheckx, pointToChecky)) printf("inside");
 	else printf("outside");
 	printf(" the Triangel\n");
 
 	// return 0
 	return 0;
+}
+
+int test2(double pointToCheckx, double pointToChecky, double m, double b)
+{
+	if (pointToChecky < m * pointToCheckx + b) {
+		return -1; // point is under line
+	}
+	else if (pointToChecky == m * pointToCheckx + b){
+		return 0;
+	}
+	else {
+		return 1; // over
+	}
+}
+
+// two points lie on the same side of a line
+int test1(double pointToCheckx, double pointToChecky, double m, double b, double lx, double ly) {
+	return (test2(pointToCheckx, pointToChecky, m, b) == test2(lx, ly, m, b));
+}
+int tritest(double vertexAx, double vertexAy, double vertexBx, double vertexBy, double vertexCx, double vertexCy, double pointToCheckx, double pointToChecky)
+{
+
+	int line1, line2, line3;
+	// line eqns
+
+	double m01 = (vertexBy - vertexAy) / (vertexBx - vertexAx);
+	// b: y - y1 = m( x - x1 ), x = 0
+	double b01 = m01 * -vertexBx + vertexBy;
+	double m02, m12, b02, b12;
+	m02 = (vertexCy - vertexAy) / (vertexCx - vertexAx);
+	m12 = (vertexCy - vertexBy) / (vertexCx - vertexBx);
+	b02 = m02 * -vertexCx + vertexCy;
+	b12 = m12 * -vertexCx + vertexCy;
+
+	// vertical line checks
+	if (vertexBx == vertexAx) {
+		line1 = ((pointToCheckx <= vertexAx) == (vertexCx <= vertexAx));
+	}
+	else {
+		line1 = test1(pointToCheckx, pointToChecky, m01, b01, vertexCx, vertexCy);
+	}
+
+	if (vertexBx == vertexCx) {
+		line2 = ((pointToCheckx <= vertexCx) == (vertexAx <= vertexCx));
+	}
+	else {
+		line2 = test1(pointToCheckx, pointToChecky, m12, b12, vertexAx, vertexAy);
+	}
+
+	if (vertexCx == vertexAx) {
+		line3 = ((pointToCheckx <= vertexAx) == (vertexBx <= vertexAx));
+	}
+	else {
+		line3 = test1(pointToCheckx, pointToChecky, m02, b02, vertexBx, vertexBy);
+	}
+
+	return line1 && line2 && line3;
+
 }
